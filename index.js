3fb9cad9fe6abd187f95b2464152acd03f962ace
Jakob Edgeworth <jakobedgeworth@gmail.com>
2025-12-23 02:09:23 -0600

    edit color display to use css grid, final touches

--- index.js
+++ index.js
@@ -1,19 +1,17 @@
 const colorsDisplay = document.getElementById('colors-display')
-const colorsHexDisplay = document.getElementById('colors-hex')
 const form = document.getElementById('picker')
 let colors = ["grey","grey","grey","grey","grey"]
 
 function renderColors(){
     for(let i=0; i < colorsDisplay.children.length; i++){
-        colorsDisplay.children[i].style.backgroundColor = colors[i]
-        colorsHexDisplay.children[i].textContent = colors[i]
+        colorsDisplay.children[i].children[0].style.backgroundColor = colors[i]
+        colorsDisplay.children[i].children[1].textContent = colors[i]
     }
 }
 
 async function getColors(hex, mode){
     const res = await fetch(`https://www.thecolorapi.com/scheme?hex=${hex}&mode=${mode}`)
     const json = await res.json()
-    console.log(json)
     for(i in json.colors){
         colors[i] = json.colors[i].hex.value
     }
@@ -24,7 +22,6 @@ function handleFormSubmit(e){
     e.preventDefault()
     const color = form.children[0].value.slice(1) //remove #
     const mode = form.children[1].value
-    console.log(color, mode)
     getColors(color,mode)
 }
 
